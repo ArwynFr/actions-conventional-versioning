a7fcf6e8460569de090fc2bdc5d0e71899a849ac
Arwyn <10365998+ArwynFr@users.noreply.github.com>
2022-09-20 13:46:07 +0200

    feat: obsolete whatif in favor of get-newVersion (#5)
    * feat: new subaction to replace whatif
    
    * fix: convert readme to asciidoc
    
    * fix(docs): convert to adoc and add get-newversion
    
    * fix(docs): github does not support include :(
    
    * fix: avoir write-host

--- functions/New-GithubRelease.ps1
+++ functions/New-GithubRelease.ps1
@@ -16,9 +16,15 @@ param (
 $Private:currentVersion = & $env:GITHUB_ACTION_PATH/functions/Get-SemVer.ps1 -RepositoryName "$RepositoryName"
 $Private:bumpType = & $env:GITHUB_ACTION_PATH/functions/Get-VersionBumpType.ps1 -CommitMessage "$CommitMessage"
 $Private:nextVersion = & $env:GITHUB_ACTION_PATH/functions/New-SemVer.ps1 -Version $Private:currentVersion -BumpType $Private:bumpType
+
 Write-Host "::set-output name=current-version::$Private:currentVersion"
 Write-Host "::set-output name=bump-type::$Private:bumpType"
 Write-Host "::set-output name=next-version::$Private:nextVersion"
+
+Write-Output "current-version: $Private:currentVersion"
+Write-Output "bump-type: $Private:bumpType"
+Write-Output "next-version: $Private:nextVersion"
+
 if ($PSCmdlet.ShouldProcess($private:nextVersion, 'gh release create')) {
     gh release create "$private:nextVersion" --generate-notes --repo "$RepositoryName" "$Pattern"
 }
