da0c043ffde2c9e20a49fd81a493495229c432da
Arwyn <10365998+ArwynFr@users.noreply.github.com>
2022-09-27 01:41:24 +0200

    fix: relative path for powershell scripts (#8)

--- functions/New-GithubRelease.ps1
+++ functions/New-GithubRelease.ps1
@@ -13,9 +13,9 @@ param (
     $Pattern
 )
 
-$Private:currentVersion = & $env:GITHUB_ACTION_PATH/functions/Get-SemVer.ps1 -RepositoryName "$RepositoryName"
-$Private:bumpType = & $env:GITHUB_ACTION_PATH/functions/Get-VersionBumpType.ps1 -CommitMessage "$CommitMessage"
-$Private:nextVersion = & $env:GITHUB_ACTION_PATH/functions/New-SemVer.ps1 -Version $Private:currentVersion -BumpType $Private:bumpType
+$Private:currentVersion = & $PSScriptRoot/Get-SemVer.ps1 -RepositoryName "$RepositoryName"
+$Private:bumpType = & $PSScriptRoot/Get-VersionBumpType.ps1 -CommitMessage "$CommitMessage"
+$Private:nextVersion = & $PSScriptRoot/New-SemVer.ps1 -Version $Private:currentVersion -BumpType $Private:bumpType
 
 Write-Host "::set-output name=current-version::$Private:currentVersion"
 Write-Host "::set-output name=bump-type::$Private:bumpType"
