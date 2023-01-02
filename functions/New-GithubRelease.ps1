1b53ec9f1559c625d5df62b182fbf8c04c140c1c
Arwyn <10365998+ArwynFr@users.noreply.github.com>
2023-01-02 20:44:08 +0100

    fix: set-output deprecated

--- functions/New-GithubRelease.ps1
+++ functions/New-GithubRelease.ps1
@@ -17,9 +17,9 @@ $Private:currentVersion = & $PSScriptRoot/Get-SemVer.ps1 -RepositoryName "$Repos
 $Private:bumpType = & $PSScriptRoot/Get-VersionBumpType.ps1 -CommitMessage "$CommitMessage"
 $Private:nextVersion = & $PSScriptRoot/New-SemVer.ps1 -Version $Private:currentVersion -BumpType $Private:bumpType
 
-Write-Host "::set-output name=current-version::$Private:currentVersion"
-Write-Host "::set-output name=bump-type::$Private:bumpType"
-Write-Host "::set-output name=next-version::$Private:nextVersion"
+Write-Host "current-version=$Private:currentVersion" >> $env:GITHUB_OUTPUT
+Write-Host "bump-type=$Private:bumpType" >> $env:GITHUB_OUTPUT
+Write-Host "next-version=$Private:nextVersion" >> $env:GITHUB_OUTPUT
 
 Write-Output "current-version: $Private:currentVersion"
 Write-Output "bump-type: $Private:bumpType"
