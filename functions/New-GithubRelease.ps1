4501584514b7a249c71cda41a5b1610379595bb6
Arwyn <10365998+ArwynFr@users.noreply.github.com>
2023-04-17 22:39:09 +0200

    fix: whatif prevents write-output (#14)

--- functions/New-GithubRelease.ps1
+++ functions/New-GithubRelease.ps1
@@ -13,19 +13,12 @@ param (
     $Pattern
 )
 
-$Private:currentVersion = & $PSScriptRoot/Get-SemVer.ps1 -RepositoryName "$RepositoryName"
-$Private:bumpType = & $PSScriptRoot/Get-VersionBumpType.ps1 -CommitMessage "$CommitMessage"
-$Private:nextVersion = & $PSScriptRoot/New-SemVer.ps1 -Version $Private:currentVersion -BumpType $Private:bumpType
-
-Write-Host "current-version=$Private:currentVersion" >> $env:GITHUB_OUTPUT
-Write-Host "bump-type=$Private:bumpType" >> $env:GITHUB_OUTPUT
-Write-Host "next-version=$Private:nextVersion" >> $env:GITHUB_OUTPUT
+$private:nextVersion = & $PSScriptRoot/Get-NewVersion.ps1 -RepositoryName $RepositoryName -CommitMessage $CommitMessage
 
 if ($PSCmdlet.ShouldProcess($private:nextVersion, 'gh release create')) {
     if (($null -eq $Pattern) -or ('' -eq $Pattern)) {
         gh release create "$private:nextVersion" --generate-notes --repo "$RepositoryName"
-    }
-    else {
+    } else {
         gh release create "$private:nextVersion" --generate-notes --repo "$RepositoryName" (Get-Item "$Pattern")
     }
 }
