887b76ded147331defe420782f93693d8a6a9486
Arwyn <10365998+ArwynFr@users.noreply.github.com>
2023-05-22 13:31:54 +0200

    fix: upload release assets separately

--- functions/New-GithubRelease.ps1
+++ functions/New-GithubRelease.ps1
@@ -16,9 +16,8 @@ param (
 $private:nextVersion = & $PSScriptRoot/Get-NewVersion.ps1 -RepositoryName $RepositoryName -CommitMessage $CommitMessage
 
 if ($PSCmdlet.ShouldProcess($private:nextVersion, 'gh release create')) {
-    if (($null -eq $Pattern) -or ('' -eq $Pattern)) {
-        gh release create "$private:nextVersion" --generate-notes --repo "$RepositoryName"
-    } else {
-        gh release create "$private:nextVersion" --generate-notes --repo "$RepositoryName" (Get-Item "$Pattern")
+    gh release create "$private:nextVersion" --generate-notes --repo "$RepositoryName"
+    if (-Not [string]::IsNullOrEmpty($Pattern)) {
+        gh release upload "$private:nextVersion" --repo "$RepositoryName" (Get-Item "$Pattern")
     }
 }
