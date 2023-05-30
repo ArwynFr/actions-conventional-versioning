be1fe050ff9f248f1db38587bc067972b8e3b209
Arwyn <10365998+ArwynFr@users.noreply.github.com>
2023-05-30 10:22:33 +0200

    feat!: extended conventional commits (#19)
    * fix: comply to conventional commits by default
    
    * fix: action path
    
    * fix: add github token
    
    * fix: separate steps
    
    * fix: use version from output
    
    * fix: missing repo name in release create step
    
    * fix: self release workflow configuration
    
    * chore: add PR title verification
    
    * style: trailing spaces
    
    * chore: change default branch name to main

--- functions/New-GithubRelease.ps1
+++ functions/New-GithubRelease.ps1
@@ -3,21 +3,19 @@ param (
     [Parameter(Mandatory)]
     [string]
     $RepositoryName,
-
-    [Parameter()]
-    [string]
-    $CommitMessage,
+    
+    [Parameter(Mandatory)]
+    [semver]
+    $NextVersion,
 
     [Parameter()]
     [string]
     $Pattern
 )
 
-$private:nextVersion = & $PSScriptRoot/Get-NewVersion.ps1 -RepositoryName $RepositoryName -CommitMessage $CommitMessage
-
-if ($PSCmdlet.ShouldProcess($private:nextVersion, 'gh release create')) {
-    gh release create "$private:nextVersion" --generate-notes --repo "$RepositoryName"
+if ($PSCmdlet.ShouldProcess($NextVersion, 'gh release create')) {
+    gh release create "$NextVersion" --generate-notes --repo "$RepositoryName"
     if (-Not [string]::IsNullOrEmpty($Pattern)) {
-        gh release upload "$private:nextVersion" --repo "$RepositoryName" (Get-Item "$Pattern")
+        gh release upload "$NextVersion" --repo "$RepositoryName" (Get-Item "$Pattern")
     }
 }
